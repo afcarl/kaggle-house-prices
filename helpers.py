b2306f2b9382dca72beb82bcb20000781021c242
robmarkcole <robmarkcole@gmail.com>
2018-01-27 11:44:04 +0000

    Initial

---
+++ helpers.py
@@ -0,0 +1,12 @@
+"""
+Convenience functions for kaggle house prices comp.
+"""
+
+
+def get_info(query):
+    """Convenience to print description"""
+    f = open('data/data_description.txt', 'r')
+    lines = f.readlines()
+    for line in lines:
+        if line.startswith(query):
+            print(line)
