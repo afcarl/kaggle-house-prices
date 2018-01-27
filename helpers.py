9a945445a267e1b89a26d812828fff8795a7c77d
robmarkcole <robmarkcole@gmail.com>
2018-01-27 15:09:03 +0000

    more

--- helpers.py
+++ helpers.py
@@ -1,7 +1,7 @@
 """
 Convenience functions for kaggle house prices comp.
 """
-
+import math
 
 def get_info(query):
     """Convenience to return description"""
@@ -10,3 +10,10 @@ def get_info(query):
     for line in lines:
         if line.startswith(query):
             return(line.split(":")[-1].rstrip())
+
+
+#A function to calculate Root Mean Squared Logarithmic Error (RMSLE)
+def rmsle(y, y_pred):
+    assert len(y) == len(y_pred)
+    terms_to_sum = [(math.log(y_pred[i] + 1) - math.log(y[i] + 1)) ** 2.0 for i,pred in enumerate(y_pred)]
+    return (sum(terms_to_sum) * (1.0/len(y))) ** 0.5
