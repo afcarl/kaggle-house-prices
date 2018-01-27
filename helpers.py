6fbc048c512acac5816aad26752e7b30cf18d2a6
robmarkcole <robmarkcole@gmail.com>
2018-01-27 12:24:55 +0000

    update

--- helpers.py
+++ helpers.py
@@ -4,9 +4,9 @@ Convenience functions for kaggle house prices comp.
 
 
 def get_info(query):
-    """Convenience to print description"""
+    """Convenience to return description"""
     f = open('data/data_description.txt', 'r')
     lines = f.readlines()
     for line in lines:
         if line.startswith(query):
-            return(line.split(":")[-1])
+            return(line.split(":")[-1].rstrip())
